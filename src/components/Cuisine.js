714238fd10804e1a6f46c4404d8956d6a00efa12
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2021-04-12 10:41:45 -0400

    more changes to Parent component

--- src/components/Cuisine.js
+++ src/components/Cuisine.js
@@ -1,1 +1,17 @@
 // JavaScript source code
+
+import React from "react"
+
+class Cuisine extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.checkedCuisine.map(cuisine => (
+          <li>{cuisine.title}</li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+export default Cuisine
--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -1,49 +1,118 @@
 import React from "react"
 import Header from "./Header"
 import SearchInput from "./SearchInput"
+import Cuisine from "./Cuisine"
+import Price from "./Price"
+import Reviews from "./Reviews"
+import MileRadius from "./MileRadius"
+import Hours from "./Hours"
 
 class ParentComponent extends React.Component {
     state = {
         location: "",
-        checkedItems: [
+        mileRadius: "",
+        checkedPrice: [
           {
             id: 1,
-            title: "Hours",
-            completed: true
+            title: "$",
+            completed: false
           },
           {
             id: 2,
-            title: "Location",
+            title: "$$",
             completed: false
           },
           {
             id: 3,
-            title: "MileRadius",
+            title: "$$$",
+            completed: false
+          }
+        ],
+        checkedHours: [
+          {
+            id: 1,
+            title: "Breakfast",
+            completed: false
+          },
+          {
+            id: 2,
+            title: "Lunch",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "Dinner",
+            completed: false
+          }
+        ],
+        
+        checkedReviews: [
+          {
+            id: 1,
+            title: "OneStar",
+            completed: false
+          },
+          {
+            id: 2,
+            title: "TwoStar",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "ThreeStar",
             completed: false
           },
           {
             id: 4,
-            title: "Price",
+            title: "FourStar",
             completed: false
           },
           {
             id: 5,
-            title: "Reviews",
+            title: "FiveStar",
+            completed: false
+          }
+
+        ],
+        checkedCuisine: [
+          {
+            id: 1,
+            title: "Italian",
             completed: false
           },
           {
-            id: 6,
-            title: "TakeOut",
+            id: 2,
+            title: "Mexican",
+            completed: false
+          },
+          {
+            id: 3,
+            title: "American",
+            completed: false
+          },
+          {
+            id: 4,
+            title: "Asian",
+            completed: false
+          },
+          {
+            id: 5,
+            title: "Mediterranean",
             completed: false
           }
-        ]
+        ]      
       }
 
   render() {
     return (
       <div>
           <Header/>
-          <SearchInput location={this.state.location} />
+          <SearchInput location={this.state.location.completed} />
+          <Cuisine cuisine = {this.state.checkedCuisine.completed} />
+          <Price price = {this.state.checkedPrice.completed} />
+          <Reviews reviews = {this.state.checkedReviews.completed} />
+          <MileRadius radius = {this.state.mileRadius.completed} />
+          <Hours hours = {this.state.checkedHours.completed} />
       </div>
     )
   }
