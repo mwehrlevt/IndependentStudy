5339bd11d1b3faae65a527f787d6f0ee89d784f9
Michelle Wehrle <mwehrlevt@gmail.com>
2021-04-12 10:53:34 -0400

    Added locationInfoBox component

--- src/components/LocationInfoBox.js
+++ src/components/LocationInfoBox.js
@@ -1,1 +1,15 @@
-// JavaScript source code
+const LocationInfoBox = ({ info }) => {
+    return (
+        <div>
+            <h2>Event Location Info</h2>
+            <ul>
+                <li>NAME: <strong>{info.name}</strong></li>
+                <li>HOURS: <strong>{info.hours}</strong></li>
+            </ul>
+        </div>
+
+    )
+}
+
+
+export default LocationInfoBox
