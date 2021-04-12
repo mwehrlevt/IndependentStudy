40359a03fbd4c1c82682233e9f4b14efd5eaeccd
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2021-04-12 10:15:41 -0400

    more changes

---
+++ src/components/SearchInput.js
@@ -0,0 +1,10 @@
+import React from "react"
+class SearchInput extends React.Component{
+    render(){
+        return(
+            <li>{this.props.location}</li>
+        )
+    }
+}
+
+export default SearchInput
