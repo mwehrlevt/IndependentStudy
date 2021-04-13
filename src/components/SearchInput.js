37b3fd77015e15e28456cbd953a36ae980f85f3a
Lauren Paicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2021-04-13 11:04:48 -0400

    Got search input kind of working

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,10 +1,24 @@
-import React from "react"
-class SearchInput extends React.Component{
-    render(){
-        return(
-            <li>{this.props.location}</li>
-        )
-    }
-}
+import React, { Component } from "react"
 
+class SearchInput extends Component {
+
+    state = {
+        title: ""
+      };
+
+      onChange = e => {
+        this.setState({
+            title: e.target.value
+          });
+      };
+
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Search for resturant ..." value={this.state.title} onChange={this.onChange}/>
+        <button>Submit</button>
+      </form>
+    )
+  }
+}
 export default SearchInput
