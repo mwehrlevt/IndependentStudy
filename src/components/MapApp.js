66ce916baafc35d4c2f3169e2f517361d09bd7ba
Michelle Wehrle <mwehrlevt@gmail.com>
2021-04-15 13:37:59 -0400

    Worked on Map API requests

--- src/components/MapApp.js
+++ src/components/MapApp.js
@@ -1,6 +1,25 @@
 import Map from "./Map"
+import {useState, useEffect} from 'react'
 
 function MapApp() {
+    const [eventData, setEventData] = useState([])
+    const [loading, setLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            setLoading(true)
+            const res = await fetch('https://maps.googleapis.com/maps/api/place/textsearch/xml?query=restaurants+in+Sydney&key=AIzaSyBejaZOfwWW5yspBtDdfzPAGKIShmNyY8M')
+            const {} = await res.json()
+
+            setEventData()
+            setLoading(false)
+        }
+
+        fetchEvents()
+
+        console.log()
+    }, [])
+
     return (
         <div>
             <h1>Map!!!</h1>
