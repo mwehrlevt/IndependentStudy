4e92e611adb7efd14a34897eff8eb0f5d2804b69
Michelle Wehrle <mwehrlevt@gmail.com>
2021-04-22 11:27:28 -0400

    Got severe storms icons to show up!

--- src/components/SevereStormsLocationMarker.js
+++ src/components/SevereStormsLocationMarker.js
@@ -1,1 +1,12 @@
+import { Icon } from '@iconify/react'
+import severeStormsIcon from '@iconify/icons-mdi/weather-lightning-rainy'
 
+const SevereStormsLocationMarker = ({ lat, lng, onClick }) => {
+    return (
+        <div className="severe-storms-location-marker" onClick={onClick}>
+            <Icon icon={severeStormsIcon} className="severe-storms-icon" />
+        </div>
+    )
+}
+
+export default SevereStormsLocationMarker
