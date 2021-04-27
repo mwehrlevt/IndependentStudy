b5debb4740c399dbec085d8ac13c0f7c2a9bd37c
Michelle Wehrle <mwehrlevt@gmail.com>
2021-04-27 12:47:41 -0400

    Fixed volcano data bug

--- src/components/Map.js
+++ src/components/Map.js
@@ -10,31 +10,43 @@ const Map = ({ eventData, center, zoom }) => {
     const [locationInfo, setLocationInfo] = useState(null)
 
     const markers = eventData.map(ev => {
+        // Severe Storm data
         if (ev.categories[0].id === 10) {
             const storms = ev.geometries
 
+            return <SevereStormsLocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
+                onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+
             storms.map((value, index) => {
-                console.log(value.coordinates[1], value.coordinates[0])
+                //console.log(value.coordinates[1], value.coordinates[0])
                 return <SevereStormsLocationMarker lat={value.coordinates[1]} lng={value.coordinates[0]}
                     onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
             })
             
         }
+
+        // Fire data
         if (ev.categories[0].id === 8) {
             return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
                 onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}/>
         }
 
+        // Sea and Ice data
         if (ev.categories[0].id === 15) {
             return <SeaAndIceLocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
                 onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
         }
 
-        // TODO: FIX
-        //if (ev.categories[0].id === 12) {
-        //    return <VolcanoLocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
-        //        onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
-        //}
+        // Volcano data
+        if (ev.categories[0].id === 12 && ev.geometries[0].type === "Point") {
+            return <VolcanoLocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
+                onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+        }
+
+        if (ev.categories[0].id === 12 && ev.geometries[0].type === "Polygon") {
+            return <VolcanoLocationMarker lat={ev.geometries[0].coordinates[0][0][1]} lng={ev.geometries[0].coordinates[0][0][0]}
+                onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
+        }
 
         return null
     })
