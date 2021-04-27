3c30a01ac8b8bdfa853ca3a6ab9aa7ad03873688
Lauren Paicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2021-04-26 21:26:15 -0400

    Added input validation and fixed bugs

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -4,41 +4,85 @@ class SearchInput extends Component {
   
   state = {
     longitude: "",
-    latitude: ""
+    latitude: "",
+    longitudeError: "",
+    latitudeError: "",
   };
 
-      onChange = e => {
-        this.setState({
-          [e.target.name]: e.target.value,
-          });
-      };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-      handleSubmit = e => {
-        e.preventDefault();
-        this.props.searchCoordinatesProps(this.state.title);
-        this.setState({
-          longitude: "",
-          latitude: ""
-        });
-      };
+  validate = () => {
+    let longitudeError = ""
+    let latitudeError = ""
+    
+    if (!this.state.latitude){
+      latitudeError = "Inputs cannot be blank";
+    }
+    else if (this.state.latitude == 0){
+      latitudeError = "Input must be greater than one";
+    }
+    if (latitudeError){
+      this.setState({latitudeError});
+      return false;
+    }
+    if (!this.state.longitude){
+      longitudeError = "Inputs cannot be blank";
+    }
+    else if (this.state.longitude == 0){
+      longitudeError = "Input must be greater than one";
+    }
+    if (longitudeError){
+      this.setState({longitudeError});
+      return false;
+    }
+    return true;
+  };
 
+  handleSubmit = e => {
+    e.preventDefault();
+    const isValid = this.validate();
+    if (isValid){
+      this.props.submitSearch(this.state.longitude);
+      this.props.submitSearch(this.state.latitude);
+      this.setState({
+        longitudeError: "",
+        latitudeError: "",
+      })
+    } 
+    this.setState({
+      longitude: "",
+      latitude: "",
+    });
+  };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input type="text" 
-        placeholder="Longitude value" 
-        value={this.state.longitude}
-        name = "longitude" 
-        onChange={this.onChange}/>
-
-        <input
-        type="text"
-        placeholder="Latitude value"
-        value={this.state.latitude}
-        name="latitude"
-        onChange={this.onChange}
-        />
+      <form onSubmit = {this.handleSubmit}>
+        <div>
+          <input 
+            type = "number" 
+            placeholder = "Longitude value" 
+            value = {this.state.longitude}
+            name = "longitude" 
+            onChange = {this.onChange}
+          />
+          <div> {this.state.longitudeError} </div>
+        </div>
+        
+        <div>
+          <input
+            type="number"
+            placeholder="Latitude value"
+            value={this.state.latitude}
+            name = "latitude"
+            onChange={this.onChange}
+          />
+          <div> {this.state.latitudeError} </div>
+        </div>
         <button>Submit</button>
       </form>
     )
