b1089725a5f9233afa1f4cd1e1abcaa034b35e90
Lauren Paicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2021-05-09 22:41:03 -0400

    Added CSS and description

--- src/components/NASAcheckboxes.js
+++ src/components/NASAcheckboxes.js
@@ -6,6 +6,13 @@ function NASAcheckboxes(props) {
     return (
 
       <ul>
+        <p> Welcome to the NASA Events Search! <br></br>Displayed below is a map of various the natural events occuring
+          across the planet. <br></br>These include wildfires, severe storms, volcanos, and sea and lake ice. <br></br>
+          You can enter in longitiude and latitude values and select a type of natural<br></br> 
+          event to search for on the map below. The map will display the four various<br></br> 
+          icons to indicate what natural event you have tracked.
+        </p>
+        <p>Please select a checkbox below:</p>
         {props.checkedNASAProps.map(label => (
           <li> {label.title} <input type="checkbox" checked={props.checkedNASAProps.completed} 
           onChange={() => props.handleChangeProps(label.id)}/> 
