72870274e5b436c013bdb11af5fa0e62f92758ce
Lauren Paicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2021-05-11 11:07:55 -0400

    Partner Programming

--- src/components/NASAcheckboxes.js
+++ src/components/NASAcheckboxes.js
@@ -1,6 +1,10 @@
 // JavaScript source code
-
 import React from "react"
+import { Icon } from '@iconify/react'
+import volcanoIcon from '@iconify/icons-mdi/terrain'
+import seaIcon from '@iconify/icons-mdi/waves'
+import severeStormsIcon from '@iconify/icons-mdi/weather-lightning-rainy'
+import fireIcon from '@iconify/icons-mdi/fire-alert'
 
 function NASAcheckboxes(props) {
     return (
@@ -12,10 +16,17 @@ function NASAcheckboxes(props) {
           event to search for on the map below. The map will display the four various<br></br> 
           icons to indicate what natural event you have tracked.
         </p>
+        <div className="location-marker">
+          <Icon icon={fireIcon} className="fire-icon-pic" />
+          <Icon icon={severeStormsIcon} className="severe-storms-icon-pic" />
+          <Icon icon={volcanoIcon} className="volcano-icon-pic" /> 
+          <Icon icon={seaIcon} className="sea-icon-pic" />
+        </div>
+        
         <p>Please select a checkbox below:</p>
         {props.checkedNASAProps.map(label => (
           <li> {label.title} <input type="checkbox" checked={props.checkedNASAProps.completed} 
-          onChange={() => props.handleChangeProps(label.id)}/> 
+          onChange={() => props.handleChangeProps(label.id)} /> 
           </li>
           ))}
       </ul>
