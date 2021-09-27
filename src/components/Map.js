4fd4b99f5b820be6165f971c66b5dc736c8e9d85
Lauren Paicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2021-09-27 15:11:25 -0400

    Update Map.js for bug fixes

--- src/components/Map.js
+++ src/components/Map.js
@@ -7,15 +7,17 @@ import SeaAndIceLocationMarker from './SeaAndIceLocationMarker'
 import VolcanoLocationMarker from './VolcanoLocationMarker'
 
 const Map = ({ checkBoxes, eventData, longitude, latitude, center, zoom }) => {
-    //console.log(checkBoxes.checkedNASAProps[1].completed);
-    console.log(longitude);
-    console.log(latitude);
-    //console.log("hello")
     const [locationInfo, setLocationInfo] = useState(null)
 
+    console.log('MapApp: checkBoxes is ' + checkBoxes);
+    console.log('MapApp: latitudeProp is ' + latitude);
+    console.log('MapApp: longitudeProp is ' + longitude);
+
+    console.log(center);
+    
     const markers = eventData.map(ev => {
         // Severe Storm data
-        if (checkBoxes.checkedNASAProps[1].completed) {
+        if (checkBoxes[1].completed) {
             if (ev.categories[0].id === 10) {
                 return <SevereStormsLocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
                     onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
@@ -24,7 +26,7 @@ const Map = ({ checkBoxes, eventData, longitude, latitude, center, zoom }) => {
         }
         
         // Fire data
-        if (checkBoxes.checkedNASAProps[0].completed) {
+        if (checkBoxes[0].completed) {
             if (ev.categories[0].id === 8) {
                 return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
                     onClick={() => setLocationInfo({ id: ev.id, title: ev.title })}/>
@@ -32,7 +34,7 @@ const Map = ({ checkBoxes, eventData, longitude, latitude, center, zoom }) => {
         }
 
         // Sea and Ice data
-        if (checkBoxes.checkedNASAProps[3].completed) {
+        if (checkBoxes[3].completed) {
             if (ev.categories[0].id === 15) {
                 return <SeaAndIceLocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
                     onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
@@ -40,7 +42,7 @@ const Map = ({ checkBoxes, eventData, longitude, latitude, center, zoom }) => {
         }
 
         // Volcano data (point type)
-        if (checkBoxes.checkedNASAProps[2].completed) {
+        if (checkBoxes[2].completed) {
             if (ev.categories[0].id === 12 && ev.geometries[0].type === "Point") {
                 return <VolcanoLocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]}
                     onClick={() => setLocationInfo({ id: ev.id, title: ev.title })} />
@@ -59,9 +61,7 @@ const Map = ({ checkBoxes, eventData, longitude, latitude, center, zoom }) => {
     return (
         <div className="map">
             <GoogleMapReact
-                bootstrapURLKeys={{
-                    key: 'AIzaSyBejaZOfwWW5yspBtDdfzPAGKIShmNyY8M'
-                }}
+                bootstrapURLKeys={{key: 'AIzaSyBejaZOfwWW5yspBtDdfzPAGKIShmNyY8M'}}
                 defaultCenter={center}
                 defaultZoom={zoom}
             >
@@ -72,12 +72,12 @@ const Map = ({ checkBoxes, eventData, longitude, latitude, center, zoom }) => {
     )
 }
 
-
 Map.defaultProps = {
     center: {
-        lat: 44.4759,
-        lng: -73.2121
+        lat: 72,
+        lng: 44
     },
     zoom: 6
 }
+
 export default Map
